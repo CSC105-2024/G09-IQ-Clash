a86f59b5020fa58ca03895e2d4f22eb25626af2f
Pal <thanapat.doun@mail.kmutt.ac.th>
2025-04-25 15:15:19 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Homepage from './pages/Homepage.jsx'
+import Homepage from './pages/Homepage.jsx';
+import UserPage from './pages/UserPage.jsx';
+import DoingQuiz from './pages/DoingQuiz.jsx';
 import Register from './pages/Register.jsx';
 import Login from './pages/Login.jsx';
 import ChoosingQuiz from './pages/ChoosingQuiz.jsx';
+import ChoosingQuiz2 from './pages/ChoosingQuiz2.jsx';
 import Leaderboard from './pages/Leaderboard.jsx';
 import Summary from './pages/Summary.jsx';
 const router = createBrowserRouter([
